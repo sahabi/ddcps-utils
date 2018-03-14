75add07a936b9ffac627706196d8dd210d1276f3
sahabi <sahabi@gmail.com>
2018-03-14 17:38:34 -0500

    added args

--- data_utility.py
+++ data_utility.py
@@ -1,7 +1,9 @@
 from pandas import read_csv
 from glob import glob
 from itertools import product
+import argparse
 import pandas as pd
+import sys
 
 def mk_files(dirs, f):
     return ['/'.join([r,ff]) for (r,ff) in product(dirs,[f])]
@@ -13,7 +15,7 @@ def globber(root_dir):
 def merge(df1, df2, on=["layer","build","time"], how="outer"):
     return pd.merge(df1, df2, on=on, how=how)
 
-def read_rename(path=f_name):
+def read_rename(f_name):
     df = read_csv(f_name)
     build = f_name.split("/")[-4].lower().replace(" ","_")
     layer = f_name.split("/")[-2].lower().replace(" ","_")
@@ -25,15 +27,43 @@ def read_rename(path=f_name):
     df["build"] = build
     return df
 
-def aggregate(path=root, sensor_files):
-
-    snsrs_f_names = ['Heaters.csv', 'Quartz Lamp Duty Cycle.csv', 'Thermocouples.csv']
-    root_dirs = ['10_11_16_build/layers','11_03_16_build/layers']
-    dirs = [item for sublist in map(globber, root_dirs) for item in sublist]
-    files = [['/'.join([r,ff]) for (r,ff) in product(dirs,[f])] for f in snsrs_f_names]
-    dfs = {f: pd.concat(map(read_rename, mk_files(dirs, f))) for f in files}
+def aggregate(root_dir):
+    sensor_f_names = ['Heaters.csv', 'Quartz Lamp Duty Cycle.csv', 'Thermocouples.csv']
+    build_dirs = ['10_11_16_build/layers','11_03_16_build/layers']
+    abs_build_dirs = [root_dir + bd for bd in build_dirs]
+    dirs = [item for sublist in map(globber, abs_build_dirs) for item in sublist]
+    dfs = {f: pd.concat(map(read_rename, mk_files(dirs, f))) for f in sensor_f_names}
     dfs = reduce(merge, dfs.itervalues())
     return dfs
 
 def smart_fillna(dfs):
     return dfs
+
+def main():
+    parser = argparse.ArgumentParser(description='Preprocessing the LAMPS build data')
+    parser.add_argument("-agg", '--aggregate', dest='agg', metavar='DIR',  nargs=1,
+            help="Aggregate all build data a single dataset"
+    parser.add_argument("-f", '--fill', dest='fillna', metavar='DIR', nargs='?',
+            default="nofill", help='Fill the empty fields')
+    parser.add_argument("-o", '--output', dest='output_name', nargs=1, default='out.csv',
+            help="Provide a custom name for the output dataset.")
+    args = parser.parse_args()
+  
+    if args.agg is not None:
+        df = aggregate(args.agg[0] + "/")
+        if args.fillna != 'nofill':
+            df = smart_fillna(df)
+        else:
+            print ("Warning: There are many fields that are empty due to a small"
+                   "missalignment of the sensors timestamps. Consider rerunning"
+                   "the program with the option -f (--fill)")
+    else:
+        if args.fillna != 'nofill':
+            if isinstance(args.fillna, str):
+                df = pd.read_csv(args.fillna)
+                df = smart_fillna(df)
+            else:
+                print("Make sure that the path to the csv file is valid")
+
+if __name__ == "__main__":
+    main()
