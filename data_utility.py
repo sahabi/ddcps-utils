239df48565a3a236058160862520b8e3e50143b6
Mohammed Alshiekh <sahabi@gmail.com>
2018-03-13 21:20:30 -0600

    made a few changes

--- data_utility.py
+++ data_utility.py
@@ -33,8 +33,6 @@ def aggregate(path=root, sensor_files):
     files = [['/'.join([r,ff]) for (r,ff) in product(dirs,[f])] for f in snsrs_f_names]
     dfs = {f: pd.concat(map(read_rename, mk_files(dirs, f))) for f in files}
     dfs = reduce(merge, dfs.itervalues())
-    dfs = merge(dfs, thermo_df)
-
     return dfs
 
 def smart_fillna(dfs):
